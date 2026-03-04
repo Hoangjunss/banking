82c022e4b660fb9b5b5b1e902a41d928ecbca9b6
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2026-03-04 15:11:19 +0700

    init(history-service): init history service dependency, application

---
+++ HistoryService/src/test/java/com/banking/HistoryService/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.HistoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
